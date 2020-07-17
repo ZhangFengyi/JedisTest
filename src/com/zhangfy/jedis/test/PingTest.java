8e9cfeb79a0a99253818bb287ae1940a3965827d
zhangfy <someonenamedzfy@vip.163.com>
2020-07-17 23:42:50 +0800

    第一次提交文件

---
+++ src/com/zhangfy/jedis/test/PingTest.java
@@ -0,0 +1,12 @@
+package com.zhangfy.jedis.test;
+
+import redis.clients.jedis.Jedis;
+
+public class PingTest {
+
+	public static void main(String[] args) {
+		Jedis jedis = new Jedis("127.0.0.1", 6379);
+		System.out.println(jedis.ping());
+	}
+
+}
